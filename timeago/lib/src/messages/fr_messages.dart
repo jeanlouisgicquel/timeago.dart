55ad2811bcc2b2a9fa859a309cd746a98f60cb64
jeanlouisgicquel <jeanlouisgicquel@users.noreply.github.com>
2020-02-10 10:43:47 +0100

    Update fr_messages.dart
    Add a third class for french messages when it needs very short messages, for example in chat app.

--- timeago/lib/src/messages/fr_messages.dart
+++ timeago/lib/src/messages/fr_messages.dart
@@ -69,3 +69,39 @@ class FrShortMessages implements LookupMessages {
   @override
   String wordSeparator() => ' ';
 }
+
+class FrVeryShortMessages implements LookupMessages {
+  @override
+  String prefixAgo() => '';
+  @override
+  String prefixFromNow() => "d'ici";
+  @override
+  String suffixAgo() => '';
+  @override
+  String suffixFromNow() => '';
+  @override
+  String lessThanOneMinute(int seconds) => "à l'instant";
+  @override
+  String aboutAMinute(int minutes) => 'une min.';
+  @override
+  String minutes(int minutes) => '$minutes min.';
+  @override
+  String aboutAnHour(int minutes) => 'une heure';
+  @override
+  String hours(int hours) => '$hours heures';
+  @override
+  String aDay(int hours) => 'un jour';
+  @override
+  String days(int days) => '$days jours';
+  @override
+  String aboutAMonth(int days) => 'un mois';
+  @override
+  String months(int months) => '$months mois';
+  @override
+  String aboutAYear(int year) => 'un an';
+  @override
+  String years(int years) => '$years ans';
+  @override
+  String wordSeparator() => ' ';
+}
+
